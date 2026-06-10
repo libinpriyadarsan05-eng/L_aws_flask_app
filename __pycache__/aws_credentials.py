87b1d81ce407e86f750e29873102a73fce3a6220
Libin-05 <libinpriyadarsan05@gmail.com>
2026-06-10 17:58:45 +0530

    Add files via upload

---
+++ __pycache__/aws_credentials.py
@@ -0,0 +1,5 @@
+host = "your-rds-endpoint"
+port = 3306
+user = "your-username"
+password = "your-password"
+db = "your-database"
--- app.py
+++ app.py
@@ -1,82 +1,39 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 14 16:39:56 2020
+Created on Sat Jul 25 12:02:51 2020
 
 @author: hp
 """
 
-from flask import Flask, render_template, request
-import key_config as keys
-import boto3 
+from flask import Flask, render_template, request, flash, redirect,url_for, jsonify, session 
+from flask import Response,send_file
 
 app = Flask(__name__)
 
-
-dynamodb = boto3.resource('dynamodb',
-                    aws_access_key_id=keys.ACCESS_KEY_ID,
-                    aws_secret_access_key=keys.ACCESS_SECRET_KEY,
-                    aws_session_token=keys.AWS_SESSION_TOKEN)
-
-from boto3.dynamodb.conditions import Key, Attr
+import rds_db as db
 
 @app.route('/')
 def index():
+    
     return render_template('index.html')
 
-
-@app.route('/signup', methods=['post'])
-def signup():
+@app.route('/insert',methods = ['post'])
+def insert():
+    
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
-        password = request.form['password']
-        
-        table = dynamodb.Table('users')
-        
-        table.put_item(
-                Item={
-        'name': name,
-        'email': email,
-        'password': password
-            }
-        )
-        msg = "Registration Complete. Please Login to your account !"
-    
-        return render_template('login.html',msg = msg)
-    return render_template('index.html')
-
-@app.route('/login')
-def login():    
-    return render_template('login.html')
-
-
-@app.route('/check',methods = ['post'])
-def check():
-    if request.method=='POST':
-        
-        email = request.form['email']
-        password = request.form['password']
-        
-        table = dynamodb.Table('users')
-        response = table.query(
-                KeyConditionExpression=Key('email').eq(email)
-        )
-        items = response['Items']
-        name = items[0]['name']
-        print(items[0]['password'])
-        if password == items[0]['password']:
-            
-            return render_template("home.html",name = name)
-    return render_template("login.html")
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
+        gender = request.form['optradio']
+        comment = request.form['comment']
+        db.insert_details(name,email,comment,gender)
+        details = db.get_details()
+        print(details)
+        for detail in details:
+            var = detail
+        return render_template('index.html',var=var)
 
 
 
 if __name__ == "__main__":
     
     app.run(debug=True)
-
