b8e45b3fa1ba94bc9bdaed97cf7b0db0fa303707
Libin-05 <libinpriyadarsan05@gmail.com>
2026-06-10 12:33:08 +0530

    Add files via upload

---
+++ key_config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 19:56:55 2020
+
+@author: hp
+"""
+
+
+ACCESS_KEY_ID='YOUR_ACCESS_KEY'
+ACCESS_SECRET_KEY='YOUR_SECRET_KEY'
+AWS_SESSION_TOKEN='YOUR_SESSION_TOKEN'
+
+
+
+
+#BUCKET_NAME='unigrade-test-bucket-test'
